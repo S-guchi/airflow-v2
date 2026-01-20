5e53fab12c706d2c2e232eadeabe5a6afebdbcf7
S-guchi <cook.shoya@gmail.com>
2026-01-20 15:54:25 +0900

    u

--- dags/external_task_sensor_example.py
+++ dags/external_task_sensor_example.py
@@ -7,26 +7,25 @@ default_args = {
     'owner': 'airflow',
     'retries': 1,
     'retry_delay': timedelta(minutes=5),
-    'start_date': datetime(2024, 1, 1),
 }
 
-dag = DAG(
-    'external_task_sensor_example',
+with DAG(
+    dag_id='external_task_sensor_example',
     default_args=default_args,
     description='ExternalTaskSensor example DAG',
-    schedule_interval=timedelta(days=1),
+    start_date=datetime(2024, 1, 1),
+    schedule='@daily',
     catchup=False,
-)
+) as dag:
 
-wait_for_upstream = ExternalTaskSensor(
-    task_id='wait_for_upstream',
-    external_dag_id='hello_world_dag',
-    external_task_id='end',
-    timeout=3600,
-    mode='reschedule',
-    dag=dag,
-)
+    wait_for_upstream = ExternalTaskSensor(
+        task_id='wait_for_upstream',
+        external_dag_id='hello_world_dag',
+        external_task_id='end',
+        timeout=3600,
+        mode='reschedule',
+    )
 
-my_task = EmptyOperator(task_id='my_task', dag=dag)
+    my_task = EmptyOperator(task_id='my_task')
 
-wait_for_upstream >> my_task
+    wait_for_upstream >> my_task
